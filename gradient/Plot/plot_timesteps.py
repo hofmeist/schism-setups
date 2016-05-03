931cf29e0136bd5b5d2eb2a997dfe6db90a1b5d2
Richard H <richard.hofmeister@hzg.de>
2016-05-03 15:06:50 +0200

    update cfl variation plot

--- gradient/Plot/plot_timesteps.py
+++ gradient/Plot/plot_timesteps.py
@@ -25,6 +25,8 @@ else:
  runs[80] = name+'_80s'
  runs[160] = name+'_160s'
 
+refname='tvd_20m'
+
 lss = ['-','--',':','-.','-..']
 tidx = 477
 
@@ -37,7 +39,9 @@ keys = sorted(runs)
 
 for dt in keys:
   folder = runs[dt]
-  label = '%d s'%dt
+  cfl = dt*10./200.
+  #label = '%d s'%dt
+  label = '%0.1f'%cfl
   print(folder)
   nc = netCDF4.Dataset('%s/outputs/1_salt.nc'%folder)
   ncv=nc.variables
@@ -61,7 +65,7 @@ for dt in keys:
     s0 = ncv['salt'][0,-1][yidx]-35.
     tax.plot(x,s0,'-',lw=3.0,color='orange',label='initial condition')
   else:
-    col = (log(int(label[:-2]))/log(5)/3.-0.3)*ones((3))
+    col = (log(int(dt))/log(5)/3.-0.3)*ones((3))
     ls = '-'#lss.pop()
     
 
@@ -71,15 +75,39 @@ for dt in keys:
   nc.close()
   vnc.close()
 
+# reference
+if True:
+  nc =  netCDF4.Dataset('%s/outputs/1_salt.nc'%refname)
+  ncv=nc.variables
+
+  t = ncv['time'][:]
+  y = ncv['y'][:]
+  yidx = where(y==0.0)
+  s = ncv['salt'][tidx,-1][yidx]-35.
+  x = ncv['x'][yidx]/1000.
+
+  vnc = netCDF4.Dataset('%s/outputs/1_hvel.nc'%refname)
+  vncv = vnc.variables
+  tnum,znum,xnum = vncv['u'].shape
+  xidx=xnum/3+xnum/6
+  v = vncv['u'][-25:,-1,xidx]
+  vt = vncv['time'][-25:]
+  hours = (vt-vt[0])/3600.
+  nc.close()
+  vnc.close()
+
+  tax.plot(x,s,'-',lw=3.0,color='red',label='reference')
+  vax.plot(hours,v,'-',color='red',lw=2.0,label='reference')
+
 tax.set_ylim(-0.1,1.1)
 tax.set_ylabel('Tracer [1/1]')
 tax.set_xlabel('km')
-tax.legend(loc='upper left',frameon=False)
+tax.legend(loc='upper left',frameon=False,ncol=2)
 
 vax.set_ylim(-0.2,0.2)
 vax.set_ylabel('surface current [m/s]')
 vax.set_xlabel('time [h]')
-vax.legend(loc='upper left',frameon=False)
+vax.legend(loc='upper left',frameon=False,ncol=2)
 
 fig.savefig('transport_comparison_%s.pdf'%name)
 show(fig)
