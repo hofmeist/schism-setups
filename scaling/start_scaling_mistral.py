8476a74282262b1bae0db50164bd8629bd21b945
Richard <richard.hofmeister@hzg.de>
2016-06-17 09:19:57 +0200

    scaling: subsequent runs

--- scaling/start_scaling_mistral.py
+++ scaling/start_scaling_mistral.py
@@ -1,3 +1,4 @@
+import commands
 import os
 import sys
 
@@ -6,8 +7,12 @@ numnodes = [4,8,16,32,64,128,256]
 numnodes = [4,8,16,32,64]
 numnodes = [4]
 
+depstring = ''
 for numnode in numnodes:
-  os.system('sbatch --ntasks=%d --ntasks-per-node=%d --error=log%04d.e --output=log%04d.o run.sh %d'%(numnode,proc_pr_node,numnode,numnode,numnode))
+  id = 'scaling%04d'%numnode
+  command = 'sbatch %s --ntasks=%d --ntasks-per-node=%d --error=log%04d.e --output=log%04d.o run_mistral.sh %s'%(depstring,numnode,proc_pr_node,numnode,numnode,id)
+  err,out = commands.getstatusoutput(command)
+  depstring = ' --dependency=afterok:%s'%out.split()[-1]
 
 
 
