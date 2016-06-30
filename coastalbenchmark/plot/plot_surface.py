7039e75170b6779fa92f5ed485fa4faf6c652fdd
Richard H <richard.hofmeister@hzg.de>
2016-06-30 15:08:43 +0200

    cb: working plot surface

--- coastalbenchmark/plot/plot_surface.py
+++ coastalbenchmark/plot/plot_surface.py
@@ -3,7 +3,7 @@ import netCDF4
 import sys
 
 if len(sys.argv)>2:
-  var = sys.argv[2]
+  varname = sys.argv[2]
 else:
   print('  usage: plot_surface.py file.nc variable')
   sys.exit(1)
@@ -11,22 +11,41 @@ else:
 nc = netCDF4.Dataset(sys.argv[1])
 ncv = nc.variables
 
-x = ncv['SCHISM_hgrid_nodes_x'][:]
-y = ncv['SCHISM_hgrid_nodes_y'][:]
+x = ncv['SCHISM_hgrid_node_x'][:]
+y = ncv['SCHISM_hgrid_node_y'][:]
 sigma = ncv['sigma'][:]
 nsigma = len(sigma)
 bidx = ncv['node_bottom_index'][:]
-nv = ncv['SCHISM_hgrid_face_nodes'][:]
+nv = ncv['SCHISM_hgrid_face_nodes'][:,:3]-1
 time = ncv['time'][:]/86400. # d
 
 
-var = ncv[var]
-time = array([0.0])
+var = ncv[varname]
+#time = array([0.0])
+cmap = cm.YlGnBu_r
+cmap.set_under(color='w')
+
+def mask_triangles(masknodes,nv):
+  idx = where(masknodes)[0]
+  nvmask = []
+  for nodes in nv:
+    if any(in1d(nodes,idx)):
+      nvmask.append(True)
+    else:
+      nvmask.append(False)
+  return asarray(nvmask)
 
 for tidx,t in enumerate(time):
   figure()
-  tripcolor(x,y,nv-1,var[tidx,-1,:].squeeze(),cmap=cmap,rasterized=True)
-  savefig('%s_surface_%05d.png'%(var,tidx),dpi=300)
+  v = var[tidx,-1,:].squeeze()
+  #mask = v == -99.
+  #mask = mask_triangles(mask,nv)
+  tripcolor(x,y,nv,v,cmap=cmap,rasterized=True)
+  clim(0,30)
+  cb=colorbar()
+  cb.ax.set_title('%s\n'%(ncv[varname].long_name),size=10.)
+  savefig('%s_surface_%05d.jpg'%(varname,tidx),dpi=100)
+  #show()
   close()
 
 
