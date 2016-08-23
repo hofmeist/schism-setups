16871e09809af0e8ed502c2431357287ee3a75f2
Richard H <richard.hofmeister@hzg.de>
2016-08-23 15:49:14 +0200

    calculate_rotation: commented plotting

--- scripts/calculate_rotation.py
+++ scripts/calculate_rotation.py
@@ -25,10 +25,15 @@ if use_proj:
   f = open('windrot_geo2proj.gr3','w')
   f.write('windrot_geo2proj.gr3\n')
   f.write('%d %d\n'%(setup.nelements,setup.nnodes))
+  angles=[]
   for n,x,y,lo,la in zip(setup.inodes,setup.x,setup.y,setup.lon,setup.lat):
     u,v = m.rotate_vector(asarray([0.0]),asarray([1.0]),asarray([lo]),asarray([la]))
     angle = arcsin(float(u))*180/pi
     f.write('%d %0.6f %0.6f %0.2f\n'%(n,x,y,angle))
+    angles.append(angle)
   f.close()
 
-
+# maybe plot figure:
+#figure()
+#tripcolor(setup.x,setup.y,asarray(setup.nv)-1,angles)
+#colorbar()
