6428b2b36b0e33544b0f1e2ab16211872763d3e8
Richard H <richard.hofmeister@hzg.de>
2016-08-23 15:39:05 +0200

    scripts: add calculate_rotation for winds

--- nwshelf/grid/README.md
+++ nwshelf/grid/README.md
@@ -5,4 +5,5 @@ gmsh -2 coast.geo
 python ../../scripts/msh2gr3.py coast.msh
 python ../../scripts/bathymetry_to_hgrid.py # creates xyd_bathymetry.pickle
 python ../../scripts/xyd_to_hgrid.py
+python ../../scripts/calculate_rotation.py
 
---
+++ scripts/calculate_rotation.py
@@ -0,0 +1,34 @@
+from pylab import *
+import pickle
+import numpy as np
+from schism import *
+
+try:
+  f = open('coast_proj.pickle','rb')
+  m,=pickle.load(f)
+  f.close()
+  use_proj=True
+except:
+  use_proj=False
+
+# get hgrid information
+print('  read setup')
+setup = schism_setup()
+
+if use_proj:
+  # get lon/lat of nodes
+  print('  map coordinates')
+  setup.lon,setup.lat = m(setup.x,setup.y,inverse=True)
+
+  # calculate rotation angles for winds:
+  print('  write rotation angles for winds')
+  f = open('windrot_geo2proj.gr3','w')
+  f.write('windrot_geo2proj.gr3\n')
+  f.write('%d %d\n'%(setup.nelements,setup.nnodes))
+  for n,x,y,lo,la in zip(setup.inodes,setup.x,setup.y,setup.lon,setup.lat):
+    u,v = m.rotate_vector(asarray([0.0]),asarray([1.0]),asarray([lo]),asarray([la]))
+    angle = arcsin(float(u))*180/pi
+    f.write('%d %0.6f %0.6f %0.2f\n'%(n,x,y,angle))
+  f.close()
+
+
