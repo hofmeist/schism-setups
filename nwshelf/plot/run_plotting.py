4bf853d03ca034d75616877f7c79d2e1c37c63a6
Richard Hofmeister <g260078@mlogin101.hpc.dkrz.de>
2016-09-26 09:53:15 +0200

    nwshelf: timestep back to 120s, modified plotting job

--- nwshelf/plot/run_plotting.py
+++ nwshelf/plot/run_plotting.py
@@ -20,8 +20,9 @@ outpath=/work/gg0877/hofmeist/$1
 cd $outpath
 
 # plot surface elevation
-python $HOME/schism/setups/nwshelf/plot/plot_surface_mistral.py 29_elev.nc elev
+#python $HOME/schism/setups/nwshelf/plot/plot_surface_mistral.py 29_elev.nc elev
 python $HOME/schism/setups/nwshelf/plot/plot_surface_mistral.py 30_elev.nc elev
+python $HOME/schism/setups/nwshelf/plot/plot_bott_surf.py 29_elev.nc elev
 
 # plot initial S,T
 python $HOME/schism/setups/nwshelf/plot/plot_bott_surf.py 1_salt.nc salt 0
