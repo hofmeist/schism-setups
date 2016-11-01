0e443dfb78b23a778f10b3203ad77166d04a225e
Richard Hofmeister <g260078@mlogin102.hpc.dkrz.de>
2016-11-01 10:01:48 +0100

    nwshelf: prepare meteo forcing

--- nwshelf/scripts/create_configuration.py
+++ nwshelf/scripts/create_configuration.py
@@ -12,4 +12,7 @@ s.dump_gr3('elev.ic',const=0.0,comment='intial surface elevation')
 s.dump_gr3('rough.gr3',const=0.0001,comment='bottom roughness')
 s.dump_gr3('diffmin.gr3',const=0.000000001,comment='diffmin')
 s.dump_gr3('diffmax.gr3',const=0.1,comment='diffmax')
+s.dump_gr3('albedo.gr3',const=0.0,comment='albedo')
+s.dump_gr3('watertype.gr3',const=5,comment='water type')
+
 
