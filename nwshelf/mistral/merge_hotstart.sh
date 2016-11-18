180045474b9d633f7ac82f1374512d535c803af9
Richard Hofmeister <g260078@mlogin105.hpc.dkrz.de>
2016-11-18 13:09:20 +0100

    nwshelf: add combining script for hotstart on mistral

---
+++ nwshelf/mistral/merge_hotstart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=mergeschism     # Specify job name
+#SBATCH --comment="SCHISM postprocessing"
+#SBATCH --partition=shared   # Specify partition name
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:20:00        # Set a limit on the total run time
+#SBATCH --wait-all-nodes=1     # start job, when all nodes are available
+#SBATCH --mail-type=FAIL       # Notify user by email in case of job failure
+#SBATCH --mail-user=richard.hofmeister@hzg.de  # Set your e−mail address
+#SBATCH --account=gg0877       # Charge resources on this project account
+#SBATCH --output=log_hostart_merge.o    # File name for standard output
+#SBATCH --error=log_hotstart_merge.e     # File name for standard error output
+
+id=nwshelf$1
+mstr=$2
+
+cd /scratch/g/$USER/schism-results/$id/outputs
+# combine hotstart
+$HOME/schism/v5.3/newbuild/bin/combine_hotstart6 -p 1080 -t 2
+ 
