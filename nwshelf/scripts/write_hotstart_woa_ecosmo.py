030cff6dfea918800d21f03cc45b066c41dbedb4
Richard Hofmeister <richard.hofmeister@hzg.de>
2018-04-01 16:16:32 +0200

    nwshelf: added script for hotstart interpolation

--- nwshelf/scripts/write_hotstart_woa_ecosmo.py
+++ nwshelf/scripts/write_hotstart_woa_ecosmo.py
@@ -112,6 +112,8 @@ class ecosmo():
     self.lat2,self.lon2 = meshgrid(self.lat,self.lon)
 
     self.use_depth_slices=True
+    self.prefetched=True 
+    self.tv={}
 
     if not(self.use_depth_slices):
       self.d3,self.lat3,self.lon3 = meshgrid(self.d,self.lat*100.,self.lon*100.,indexing='ij')
@@ -139,7 +141,60 @@ class ecosmo():
         vlat = self.lat2[self.valmask[ik]]
         self.tree[ik] = cKDTree(zip(vlon,vlat))
 
-  def interpolate(self,varname,depths,nodelon,nodelat,bidx=1):
+  def prefetch_for_interpolation(self,tracers):
+    if tracers==None:
+      print('  ecosmo prefetch: give list of tracers')
+      return
+    for tr in tracers:
+      self.tv[tr]={}
+      for didx,d in enumerate(self.d):
+        self.tv[tr][didx] = self.sv[tr][self.tidx,didx][self.valmask[didx]]
+    self.prefetched=True 
+    self.prefetched_tracers=tracers
+ 
+
+  def interpolate(self,depths,nodelon,nodelat,bidx=1,tracers=None,factors=None):
+    # start
+    if tracers==None:
+      print('  ecosmo interpolate: give list of tracers')
+      return
+
+    val = zeros((len(depths),len(tracers)))
+    if factors==None:
+      factors={key: 1.0 for key in tracers}
+
+    if True:
+     for ik,ndepth in enumerate(depths[bidx-1:]):
+      # find vertical layer in climatology
+      if self.use_depth_slices:
+        didx = np.abs(self.d - ndepth).argmin()
+
+        if False:
+          dist,inds = self.tree[didx].query((nodelon,nodelat),k=4)
+          newdidx = didx
+        else:
+          dist,inds = self.tree[didx].query((nodelon,nodelat),k=4)
+          sdist,sinds = self.tree[0].query((nodelon,nodelat),k=4)
+          newdidx=didx
+          while sum(dist)>2.0*sum(sdist):
+            newdidx=newdidx-1
+          #print('  sum(dist) ratio: %0.2f, going one layer upwards to %d'%(sum(dist)/sum(sdist),newdidx))
+            dist,inds = self.tree[newdidx].query((nodelon,nodelat),k=4)
+        w = 1 / dist
+        scaling = 1.0/np.sum(w,axis=0)
+        # this step seems most time-consuming
+        if self.prefetched:
+          for trnum,trname in enumerate(tracers):
+            val[bidx-1+ik,trnum] = factors[trname]*scaling*np.sum(w*self.tv[trname][newdidx][inds],axis=0)
+        else:
+          #masked_inds = where(self.sv['no3'][self.tidx,newdidx][self.valmask[newdidx]][inds]>-9999.)
+          masked_inds = self.valmask[newdidx]
+          for trnum,trname in enumerate(tracers):
+            val[bidx-1+ik,trnum] = factors[trname]*scaling*np.sum(w*self.sv[trname][self.tidx,newdidx][masked_inds][inds],axis=0)
+
+    return (val)
+
+  def interpolate_var(self,varname,depths,nodelon,nodelat,bidx=1):
     # start
     val = zeros((len(depths),))
 
@@ -162,7 +217,8 @@ class ecosmo():
 
 
 nws = schism_setup()
-oa = woa(ncfile=sys.argv[1])
+#oa = woa(ncfile=sys.argv[1])
+oa = woa(ncfile='/work/gg0877/hofmeist/nwshelf/input/woa13_decav_04v2.nc')
 e  = ecosmo(ncfile='/work/gg0877/hofmeist/nwshelf/input/ecosmoII.2012-01-01.nc')
 
 import os.path
@@ -178,16 +234,25 @@ else:
 
   tracers=['no3','nh4','pho','sil','oxy','dia','fla','bg','microzoo','mesozoo','det','opa','dom']
   ecosmo_tracers=['no3','nh4','po4','sio','o2','diat','flags','cyan','zoos','zool','det','det','dom'] #'opa'?
+  #ecosmo_tracers=[] 
   tracer_conc_const={'no3':5.0, \
                    'nh4':0.1, \
                    'pho':0.3, \
+                   'po4':0.3, \
                    'sil':5.0, \
+                   'sio':5.0, \
                    'oxy':85.0, \
+                   'o2':85.0, \
                    'dia':1.0e-6, \
+                   'diat':1.0e-6, \
                    'fla':1.0e-6, \
+                   'flags':1.0e-6, \
                    'bg':1.0e-6, \
+                   'cyan':1.0e-6, \
                    'microzoo':1.0e-6, \
                    'mesozoo':1.0e-6, \
+                   'zoos':1.0e-6, \
+                   'zool':1.0e-6, \
                    'det':2.0, \
                    'opa':2.0, \
                    'dom':3.0}
@@ -199,13 +264,21 @@ else:
   tracer_factor={'no3': redf1*redf6,\
                'nh4': redf1*redf6,\
                'pho': redf2*redf6,\
+               'po4': redf2*redf6,\
                'sil': redf3*redf6,\
+               'sio': redf3*redf6,\
                'oxy': 1.0,\
+               'o2': 1.0,\
                'dia': redf1*redf6,\
+               'diat': redf1*redf6,\
                'fla': redf1*redf6,\
+               'flags': redf1*redf6,\
                'bg': redf1*redf6,\
+               'cyan': redf1*redf6,\
                'microzoo': redf1*redf6,\
                'mesozoo': redf1*redf6,\
+               'zoos': redf1*redf6,\
+               'zool': redf1*redf6,\
                'det': redf1*redf6,\
                'opa': redf3*redf6,\
                'dom': redf1*redf6}
@@ -226,20 +299,31 @@ else:
 
   tr_nd = zeros((nws.znum,2+len(ecosmo_tracers)))
   nws.create_hotstart(ntracers=2+len(ecosmo_tracers))
+  e.prefetch_for_interpolation(tracers=ecosmo_tracers)
+  print('  prefetched tracers:')
+  print(e.prefetched_tracers)
+  print(e.tv.keys())
+  print(e.tv['no3'].keys())
 
   # create t,s fields:
   for nodeid,nodelon,nodelat,d in zip(nws.inodes,nws.lon,nws.lat,nws.depths):
     if (nodeid%10000) == 0:
-      print('  interpolate i = %d'%i)
-    #if i == 5000:
+      print('  interpolate i = %d'%nodeid)
+      nws.hotstart_nc.sync()
+
+    #if nodeid == 2000:
     #  break 
     depths = nws.vgrid[nodeid].filled(-1)*d
     bidx = nws.bidx[nodeid]
     t,s = oa.interpolate(depths,nodelon,nodelat,bidx=1)
     tr_nd[:,0] = t
     tr_nd[:,1] = s
-    for itr,tr in enumerate(ecosmo_tracers):
-      tr_nd[:,2+itr] = ecosmo_factors[tr]*e.interpolate(tr,depths,nodelon,nodelat,bidx=1)
+    # if interpolate from ecosmoII dataset
+    if False:
+      tr_nd[:,2:] = e.interpolate(depths,nodelon,nodelat,bidx=1,tracers=ecosmo_tracers,factors=ecosmo_factors)
+    else:
+      for trnum,trname in enumerate(ecosmo_tracers):
+        tr_nd[:,2+trnum] = tracer_factor[trname]*tracer_conc_const[trname]
 
     nws.write_hotstart_tracers_on_nodes(nodeid,tr_nd)
 
