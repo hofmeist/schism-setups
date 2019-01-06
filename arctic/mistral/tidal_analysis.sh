6895ac21282ffcb8cc7d23d923d428098b6a3131
Richard Hofmeister <richard.hofmeister@hzg.de>
2019-01-06 21:37:25 +0100

    arctic: less cpus for tidal analysis

--- arctic/mistral/tidal_analysis.sh
+++ arctic/mistral/tidal_analysis.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=tideanalysis     # Specify job name
 #SBATCH --comment="SCHISM postprocessing"
 #SBATCH --partition=compute2   # Specify partition name
-#SBATCH --ntasks=36
-#SBATCH --ntasks-per-node=36
-#SBATCH --time=02:00:00        # Set a limit on the total run time
+#SBATCH --ntasks=30
+#SBATCH --ntasks-per-node=30
+#SBATCH --time=00:60:00        # Set a limit on the total run time
 #SBATCH --wait-all-nodes=1     # start job, when all nodes are available
 #SBATCH --mail-type=FAIL       # Notify user by email in case of job failure
 #SBATCH --mail-user=richard.hofmeister@hzg.de  # Set your e−mail address
@@ -16,7 +16,8 @@
 module unload python
 module load python
 
-cd /work/gg0877/hofmeist/arctic/arctic007/2012-02
+opath=$1
+cd $opath
 
-python $HOME/schism/setups/arctic/scripts/do_tideanalysis.py schout_2.nc 36
+python $HOME/schism/setups/arctic/scripts/do_tideanalysis.py schout_2.nc 30
 
