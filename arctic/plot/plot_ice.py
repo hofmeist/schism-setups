9ffe2492d10010a110957b396f1067224b13264d
Richard Hofmeister <richard.hofmeister@hzg.de>
2019-01-06 21:31:32 +0100

    arctic: new plotting scripts

---
+++ arctic/plot/plot_ice.py
@@ -0,0 +1,165 @@
+import netCDF4
+import sys
+import matplotlib
+matplotlib.use('Agg')
+from mpl_toolkits.basemap import Basemap
+from pylab import *
+import pickle,os
+from netcdftime import utime
+import argparse
+
+def replace_superscripts(s):
+  r = unicode(s.replace('^3',u'\u00b3'))
+  r = unicode(r.replace('^2',u'\u00b2'))
+  return r
+
+parser = argparse.ArgumentParser()
+parser.add_argument('ncfile', help='netcdf file')
+parser.add_argument('varname',help='variable name')
+parser.add_argument('-tidx', help='time index')
+parser.add_argument('-vrange', help='values range [-vrange vmin,vmax]')
+parser.add_argument('-title', help='title of colorbar')
+parser.add_argument('-units', help='unit string')
+parser.add_argument('-scalefactor', help='linear scaling factor')
+args = parser.parse_args()
+
+if args.tidx is not None:
+  tidx=int(args.tidx)
+else:
+  tidx = -1
+
+if 'vrange' is not None:
+  try:
+    s1,s2 = args.vrange.split(',')
+    vmin,vmax = float(s1),float(s2)
+    uselim=True
+  except:
+    uselim=False
+else:
+  uselim=False
+
+if args.title is not None:
+  titlestr=unicode(args.title)
+else:
+  titlestr=args.varname
+
+if args.units is not None:
+  unitsstr=replace_superscripts(args.units)
+else:
+  unitsstr=''
+
+if args.scalefactor is not None:
+  fac = float(args.scalefactor)
+else:
+  fac=1.0
+
+nc = netCDF4.Dataset(args.ncfile)
+ncv = nc.variables
+varname = args.varname
+
+lon = ncv['SCHISM_hgrid_node_x'][:]
+lat = ncv['SCHISM_hgrid_node_y'][:]
+#sigma = ncv['sigma'][:]
+#nsigma = len(sigma)
+#bidx = ncv['node_bottom_index'][:]
+nv = ncv['SCHISM_hgrid_face_nodes'][:,:3]-1
+time = ncv['time'][:] # s
+ut = utime(ncv['time'].units)
+dates = ut.num2date(time)
+
+# set map boundaries
+xl,yl = (-15.0,60.0)
+xu,yu = (100,75.0)
+
+if os.path.isfile('plotproj.pickle'):
+    (proj,)=np.load('plotproj.pickle')
+else:
+    if False:
+      proj = Basemap(projection='npstere',boundinglat=55.,lon_0=0.0,resolution='i')
+    else:
+      proj = Basemap(projection='lcc',
+                       resolution='i',area_thresh=10.0,
+                        llcrnrlon=xl,
+                        llcrnrlat=yl,
+                        urcrnrlon=xu,
+                        urcrnrlat=yu,
+                        lat_0=70.0,
+                        lon_0=20.0)
+
+    f=open('plotproj.pickle','wb')
+    pickle.dump((proj,),f)
+    f.close()
+
+x,y = proj(lon,lat)
+
+var = ncv[varname]
+if varname!='ICE_tracer_1':
+  maskvar = ncv['ICE_tracer_1']
+else:
+  maskvar=var
+
+#time = array([0.0])
+cmap = cm.jet
+cmap=cm.BuPu
+#cmap.set_under(color='w')
+
+def mask_triangles(masknodes,nv):
+  idx = where(masknodes)[0]
+  nvmask = []
+  for nodes in nv:
+    if any(in1d(nodes,idx)):
+      nvmask.append(True)
+    else:
+      nvmask.append(False)
+  return asarray(nvmask)
+
+if tidx >= 0:
+  dates = [dates[tidx],]
+  tidx_offset=tidx
+else:
+  tidx_offset=0
+
+os.system('mkdir -p jpgs/%s'%varname)
+for tidx,t in enumerate(dates):
+  vs = var[tidx+tidx_offset].squeeze()
+  vmask = maskvar[tidx+tidx_offset].squeeze()
+  emask = vmask[nv].mean(axis=1)
+  plot_surface=True
+  plot_bottom=False
+  if plot_surface:
+    fig=figure()
+    fig.subplots_adjust(left=0.0,right=1.0,bottom=0.0,top=1.0)
+    if varname=='elev':
+      cmap=cm.RdYlGn
+      cmap.set_under('gray')
+    tripcolor(x,y,nv,vs,mask=emask<0.02,cmap=cmap,rasterized=True)
+    if varname=='salt':
+      clim(5,35)
+      cbtitle='surface salinity'
+    elif varname=='temp':
+      clim(1,vs.max())
+      cbtitle=u'surface temperature [\u00b0C]'
+    elif varname=='elev':
+      clim(-2,2)
+      cbtitle='ssh [m]'
+    else:
+      if uselim:
+        clim(vmin,vmax)
+      else:
+        clim(0,vs.max())
+    cbtitle = titlestr
+    if uselim:
+      clim(vmin,vmax)
+    proj.drawcoastlines()
+    proj.fillcontinents((0.9,0.9,0.8))
+    cax = axes([0.03,0.2,0.25,0.03])
+    cb=colorbar(cax=cax,orientation='horizontal')
+    for ll in cax.get_xmajorticklabels():
+      ll.set_rotation(60.)
+    cb.set_label(u'%s %s'%(cbtitle,unitsstr))
+    tstring = t.strftime('%Y%m%d-%H%M')
+    savefig('jpgs/%s/ice_%s_%s.jpg'%(varname,varname,tstring),dpi=200)
+    close()
+
+
+
---
+++ arctic/plot/plot_obstransect.py
@@ -0,0 +1,255 @@
+import matplotlib
+matplotlib.use('Agg')
+import sys
+sys.path.append('/pf/g/g260078/pythoninstall/pythonlib')
+from plotting import cm_Parula
+
+from pylab import *
+import netCDF4
+from scipy.spatial import cKDTree 
+import os
+import argparse
+from netcdftime import utime
+import numpy as np
+import cPickle as pickle
+
+def replace_superscripts(s):
+  r = unicode(s.replace('^3',u'\u00b3'))
+  r = unicode(r.replace('^2',u'\u00b2'))
+  r = unicode(r.replace('^0',u'\u00b0'))
+  return r
+
+parser = argparse.ArgumentParser()
+parser.add_argument('ncfile', help='netcdf file')
+parser.add_argument('varname',help='variable name')
+parser.add_argument('-tidx', help='time index')
+parser.add_argument('-vrange', help='values range [-vrange vmin,vmax]')
+parser.add_argument('-title', help='title of colorbar')
+parser.add_argument('-units', help='unit string')
+parser.add_argument('-fname', help='file name string')
+parser.add_argument('-scalefactor', help='linear scaling factor')
+args = parser.parse_args()
+
+if args.tidx is not None:
+  tidx=int(args.tidx)
+else:
+  tidx = -1
+
+if 'vrange' is not None:
+  try:
+    s1,s2 = args.vrange.split(',')
+    vmin,vmax = float(s1),float(s2)
+    uselim=True
+  except:
+    uselim=False
+else:
+  uselim=False
+
+if args.title is not None:
+  titlestr=unicode(args.title)
+else:
+  titlestr=args.varname
+
+if args.units is not None:
+  unitsstr=replace_superscripts(args.units)
+else:
+  unitsstr=''
+
+if args.scalefactor is not None:
+  fac = float(args.scalefactor)
+else:
+  fac=1.0
+
+if args.fname is not None:
+  fnamestr=args.fname
+else:
+  fnamestr=args.varname
+
+ncfile = args.ncfile
+varn = args.varname
+period = ncfile.split('.nc')[0].split('_')[-1]
+separate_files=False
+if separate_files:
+  zfile = 'zcor_%s'%period
+else:
+  zfile = ncfile
+
+nc = netCDF4.Dataset(zfile)
+ncv = nc.variables
+
+depth = ncv['depth'][0:]
+x = ncv['SCHISM_hgrid_node_x'][0:]
+y = ncv['SCHISM_hgrid_node_y'][0:]
+z = ncv['zcor'][0,:,0:]
+#z = ma.masked_equal(z,-9999.)
+try:
+  lmin = ncv['node_bottom_index'][0:]
+except:
+  zvar = ncv['zcor'][0]
+  lmin = zeros(x.shape,dtype='int')
+  for i in range(len(x)):
+    try:
+      lmin[i] = max(where(zvar.mask[i])[0])
+    except:
+      lmin[i] = 0
+  lmin = lmin+1
+
+znum = len(nc.dimensions['nSCHISM_vgrid_layers'])
+
+nc.close()
+
+
+znc = netCDF4.Dataset(zfile)
+zncv = nc.variables
+zv = zncv['zcor']
+
+nc = netCDF4.Dataset(ncfile)
+ncv=nc.variables
+sv = ncv[varn]
+tnum,pnum,zznum=sv.shape
+#s = ma.masked_equal(s,-9999.)
+time = ncv['time'][:] # s
+ut = utime(ncv['time'].units)
+dates = ut.num2date(time)
+
+levels=arange(znum)
+
+if False: #os.path.isfile('xy_tree.pickle'):
+    (tree,)=np.load('xy_tree.pickle')
+else:
+    tree = cKDTree(zip(x,y))
+    try:
+      f=open('xy_tree.pickle','wb')
+      pickle.dump((tree,),f)
+      f.close()
+    except:
+      pass
+
+if fnamestr=='obs':
+  onc = netCDF4.Dataset('/work/gg0877/hofmeist/arctic/echosounder/PS106_data.nc')
+  olon = onc.variables['lon'][:]
+  olat = onc.variables['lat'][:]
+  odays = onc.variables['time'][:]
+  odays = odays - odays[0]
+  odepth = onc.variables['depth'][:]
+  sv_mean = onc.variables['sv_mean'][:]
+
+  ylimmin=-760.
+  plotgrid=False
+else:
+  latmin=74.0
+  latmax=74.0
+  lonmin=-20.5
+  lonmax=60.0
+  ylimmin=-4000.
+  ntransect=500
+  plotgrid=False
+  
+
+if False:
+  if (latmax-latmin)<(lonmax-lonmin):
+    uselon=True
+  else:
+    uselon=False
+  xtrans = linspace(lonmin,lonmax,ntransect)
+  ytrans = linspace(latmin,latmax,ntransect)
+else:
+  xtrans = olon
+  ytrans = olat
+
+if tidx >= 0:
+  dates = [dates[tidx],]
+  tidx_offset=tidx
+else:
+  tidx_offset=0
+
+dist,inds = tree.query(zip(xtrans,ytrans),k=10)
+weight = 1.0/dist**2
+
+
+strans=ma.masked_equal(ones((len(dist[:,0]),znum)),0.0)
+ztrans=ones((len(dist[:,0]),znum))
+lmintrans=ones((len(dist[:,0])),dtype='int')
+
+os.system('mkdir -p %s'%varn)
+for tidx,t in enumerate(dates):
+
+  tidx = tidx+tidx_offset
+  s = sv[tidx]
+  #s = ma.masked_equal(s,-9999.)
+  z = zv[tidx]
+  zf = z.copy()
+  #z = ma.masked_equal(z,-9999.)
+  print('  tidx %03d'%tidx)
+  #lmintrans = lmin[inds].mean(axis=1)
+  for n in range(znum-1,-1,-1):
+    #strans[:,n] = np.sum(weight * s[tidx,n,inds],axis=1)/np.sum(weight,axis=1)
+    #ztrans[:,n] = np.sum(weight * z[tidx,n,inds],axis=1)/np.sum(weight,axis=1)
+    strans[:,n] = s[inds,n].mean(axis=1)
+    ztrans[:,n] = z[inds,n].mean(axis=1)
+  #for i in range(len(lmintrans)):
+  #  ztrans[i,:lmintrans[i]] = ztrans[i,lmintrans[i]]
+  
+  #strans = ma.masked_where(strans<-5.0,strans)
+
+  l2d,x2d = meshgrid(levels,odays)
+
+  figure(figsize=(25,10))
+
+  subplot(211)
+
+  # fill z-levels
+  #for n in range(len(depth)):
+  #  nidx = where(zf.mask[n,:])
+  #  zf[n,nidx] = zf[n,lmin[n]]
+
+  gridcol=(0.5,0.5,0.5)
+  cmap=cm.YlGnBu_r
+  cmap=cm.RdYlBu_r
+  sfac=1.0
+
+  if uselim:
+    climmin=vmin
+    climmax=vmax
+  else:
+    climmin=strans.min()
+    climmax=strans.max()
+  ctitle = titlestr+'\n[%s]'%unitsstr
+
+  if varn == 'diffusivity':
+    norm=matplotlib.colors.LogNorm(vmin=climmin,vmax=climmax)
+  else:
+    norm=matplotlib.colors.Normalize(vmin=sfac*climmin,vmax=sfac*climmax)
+
+  pc = pcolormesh(x2d,ztrans,sfac*strans,shading='interp',cmap=cmap,norm=norm)
+
+  if plotgrid:
+    #for yy,zz in zip(x2d,ztrans.squeeze()):
+    #  plot(yy,zz,'k-',lw=0.3,color=gridcol)
+
+    for yy,zz in zip(x2d.T,ztrans.squeeze().T):
+      plot(yy,zz,'k-',lw=0.3,color=gridcol)
+
+
+  cb=colorbar(pc)
+  cb.set_label(ctitle)
+
+  xlim(0,25)
+  xlabel('day of cruise PS106')
+  ylim(ylimmin,3.0)
+  #clim(sfac*climmin,sfac*climmax)
+  ylabel('z [m]')
+
+  subplot(212)
+
+  pcolormesh(odays,-odepth,sv_mean.T,shading='interp',cmap=cm.jet)
+  clim(-100,0.0)
+  xlim(0,25)
+  xlabel('day of cruise PS106')
+  colorbar(label='Sv')
+
+  tstring = t.strftime('%Y%m%d-%H%M')
+  savefig('jpgs/%s/transect_%s_%s_%s.png'%(varn,varn,fnamestr,tstring),dpi=300)
+  #show()
+  close()
+
